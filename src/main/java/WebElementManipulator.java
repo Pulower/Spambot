4963ff0a28de2d8f8ae75df8a72e0ecc89cbbf25
Pulower <w.dzik111@gmail.com>
2018-02-08 18:46:13 +0100

    Initial commit

---
+++ src/main/java/WebElementManipulator.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebElementManipulator {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    private void waitUntilElementIsClickable(WebElement element){
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(element));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void clickOnElement(WebElement element){
+        waitUntilElementIsClickable(element);
+        element.click();
+    }
+}
---
+++ src/test/java/SendSpam.java
@@ -0,0 +1,51 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class SendSpam {
+
+    private WebDriver driver;
+    private SendSpamPage spam;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "E:\\ChromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.google.pl");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void signUp() {
+        spam = new SendSpamPage(driver);
+        spam.gotoGmail()
+                .clickSignIn()
+                .enterEmail()
+                .clickNext()
+                .enterPassword()
+                .clickNext();
+    }
+
+    @Test(dependsOnMethods = "signUp")
+    public void sendSpam() throws IOException {
+        spam = new SendSpamPage(driver);
+        spam.createNewMessage()
+                .chooseAddress()
+                .chooseSubjectAndMessage()
+                .sendMessage();
+        Assert.assertTrue(spam.getConfirm());
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
